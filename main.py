cfefb5958598c20d993c5ba3b4b3c73e843509b2
Tamim-369 <ashiqurrahmantamim369@gmail.com>
2025-03-14 10:13:49 -0400

    adding cors

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI, File, UploadFile
 from controllers.api_controller import APIController
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="RAG Application with Gemini")
 controller = APIController()
+origins = [
+    "*"
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.post("/upload/")
 async def upload_document(file: UploadFile = File(...)):
     return await controller.upload_document(file)
